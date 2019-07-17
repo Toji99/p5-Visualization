fec75e3a823ab7b9694d0b54ecf11dfe24cfca46
Toji99 <37388441+Toji99@users.noreply.github.com>
2019-07-17 14:37:48 -0400

    updated cide

--- sketch1.js
+++ sketch1.js
@@ -1,36 +1,42 @@
 var video;
-let redArray = [];
-let greenArray = [];
-let blueArray = [];
-
+// var mic;
 function setup() {
-	index = 0;
+	i = 0
+	// avgCheck = 0
+	// mic = new p5.AudioIn();
 	createCanvas(windowWidth, windowHeight);
 	background(255);
 	video = createCapture(VIDEO);
 	video.size(0, 0, windowWidth, windowHeight);
+	// mic.start();
 	// video.hide();
 
 }
 
 function draw() {
-	console.log('here at draw');
+	//console.log('here at draw');
+	// micLevel = mic.getLevel();
 	image(video, 0, 0, windowWidth, windowHeight);
 	pixelCount = windowWidth*windowHeight;
 	video.loadPixels()
-	for (let index = 0; index <= pixelCount; index+=4){
-		redIndex = index;
-		greenIndex = 1+index;
-		greenIndex = 2+index;
-		r = video.pixels[0];
-		g = video.pixels[1];
-		b = video.pixels[2];
-		redArray[index/4] = r;
-		greenArray[index/4] = g;
-		blueArray[index/4] = b;
+	let avgColor = [video.pixels[i],video.pixels[i+1],video.pixels[i+2]]
+	for (let i = 0; i <= (pixelCount/4); i+=4){
+		avgColor[0] = (video.pixels[i]+avgColor[0])/2
+		avgColor[1] = (video.pixels[i+1]+avgColor[1])/2
+		avgColor[2] = (video.pixels[i+2]+avgColor[2])/2
 	}
-	console.log(r)
-	console.log(g)
-	console.log(b)
+	// 	redIndex = index;
+	// 	greenIndex = 1+index;
+	// 	greenIndex = 2+index;
+	// 	blueIndex = 3+index;
+	// 	r = video.pixels[redIndex];
+	// 	g = video.pixels[greenIndex];
+	// 	b = video.pixels[blueIndex];
+	// 	redArray.push(r);
+	// 	greenArray.push(g);
+	// 	blueArray.push(b);
+	// console.log(r)
+	// console.log(g)
+	console.log(avgColor)
 	video.updatePixels()
-}
+	}
